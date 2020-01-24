0eb1e05065036c5bec676b0e5d310330f6e1e616
Fertern <ferternever@gmail.com>
2020-01-24 19:35:48 +0300

    new file tree

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import Search from "./components/Search/Search";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import EditMenuContainer from "./components/EditMenu/EditMenuContainer";
-//import EditMenu from "./components/EditMenuContainer/EditMenu/EditMenu";
-//import CardTask from "./components/Cards/CardTask/CardTask";
 import Cards from "./components/Cards/Cards";
+import NavMenu from "./components/NavMenu/NavMenu";
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
-      <div className="container">
-        <div className="menu">
-          <Search />
-          <span className="ByStatus">[all *]</span>
-          <span className="ByProperty">[all *]</span>
-          <EditMenuContainer />
-        </div>
-        <Cards />
-      </div>
-      <Footer />
-    </Router>
+      <NavMenu />
+      <Cards />
+    </>
   );
 }
 
