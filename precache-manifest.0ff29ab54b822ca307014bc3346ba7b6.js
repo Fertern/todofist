0fbdf08068e0abd4e6d107830cd87c004a50140c
Fertern <ferternever@gmail.com>
2020-01-27 17:57:41 +0300

    Updates

---
+++ precache-manifest.0ff29ab54b822ca307014bc3346ba7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7500c4a40d1e0a2b7b87df4e1a4a56e4",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "1b0ee2624ca4e3774f48",
+    "url": "/todolist-react/static/css/main.6d1f92cd.chunk.css"
+  },
+  {
+    "revision": "f69e4cb263716780e5dd",
+    "url": "/todolist-react/static/js/2.1d881b65.chunk.js"
+  },
+  {
+    "revision": "4ea09ccaa3e17a5993366d885b5d939e",
+    "url": "/todolist-react/static/js/2.1d881b65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b0ee2624ca4e3774f48",
+    "url": "/todolist-react/static/js/main.abca9a9f.chunk.js"
+  },
+  {
+    "revision": "9404b024296885dca4a8",
+    "url": "/todolist-react/static/js/runtime-main.9b0aa948.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5adeddd67c32d00fb813c73db05915fb.js"
+  "/todolist-react/precache-manifest.0ff29ab54b822ca307014bc3346ba7b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todolist-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
